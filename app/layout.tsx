18043b820364852aa9cfbc1d8e1a4b55e9e01d4e
Jakob Persons <jakobep24@gmail.com>
2024-09-19 23:03:33 -0400

    nav links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import MainHeader from "@/components/main-header";
 import "./globals.css";
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<MainHeader />
+				{children}
+			</body>
 		</html>
 	);
 }
--- components/header.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function MainHeader() {
-	return (
-		<header></header>
-	);
-}
